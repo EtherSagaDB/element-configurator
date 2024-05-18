929581ba7755d71b5894fc3a02019c52ba8028d7
ArcaneDisgea <39720490+ArcaneDisgea@users.noreply.github.com>
2024-05-18 13:47:40 -0400

    refactor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,31 +42,54 @@ namespace element_configurator
             readChatColors("./elementclient.exe", content);
         }
 
+        public class chatColorOffsets
+        {
+            public int commonChatOffset = 0x0067C4BB;      // Common Chat		Default Color: ^FFFFFF 0x0067C4BC
+            public int partyChatOffset = 0x0067C4CB;       // Party Chat		Default Color: ^00FF00 0x0067C4CC
+            public int whisperChatOffset = 0x0067E983;     // Whisper Chat 	Default Color: ^FF7FFF 0x0067E984
+            public int allianceChatOffset = 0x0067E993;    // Alliance Chat	Default Color: ^00FFFF
+            public int clanChatOffset = 0x0067E943;        // Clan Chat		Default Color: ^14A9FF
+            public int worldChatOffset = 0x0067E9A3;       // World Chat		Default Color: ^FFFF00
+            public int superChatOffset = 0x0067E933;       // Super Chat		Default Color: ^A900C7
+            public int broadcastChatOffset = 0x0067C4AB;   // Broadcast Chat	Default Color: ^FF0000 0x0067C4AC
+            public int otherChatOffset = 0x0067E953;       // Other Chat		Default Color: ^B680FF
+            public int damageChatOffset = 0x0067E973;      // Damage Chat		Default Color: ^FFAE00
+            public int systemChatOffset = 0x0067E963;		// System Chat		Default Color: ^FF7F00
+        }
+
         static void readChatColors(string file, TextBox[] content)
         {
             // player usable chats 
-            int commonChatOffset = 0x0067C4BB;      // Common Chat		Default Color: ^FFFFFF 0x0067C4BC
-            int partyChatOffset = 0x0067C4CB;       // Party Chat		Default Color: ^00FF00 0x0067C4CC
-            int whisperChatOffset = 0x0067E983;     // Whisper Chat 	Default Color: ^FF7FFF 0x0067E984
-            int allianceChatOffset = 0x0067E993;    // Alliance Chat	Default Color: ^00FFFF
-            int clanChatOffset = 0x0067E943;        // Clan Chat		Default Color: ^14A9FF
-            int worldChatOffset = 0x0067E9A3;       // World Chat		Default Color: ^FFFF00
-            int superChatOffset = 0x0067E933;       // Super Chat		Default Color: ^A900C7
-            int broadcastChatOffset = 0x0067C4AB;   // Broadcast Chat	Default Color: ^FF0000 0x0067C4AC
-            int otherChatOffset = 0x0067E953;       // Other Chat		Default Color: ^B680FF
-            int damageChatOffset = 0x0067E973;      // Damage Chat		Default Color: ^FFAE00
-            int systemChatOffset = 0x0067E963;		// System Chat		Default Color: ^FF7F00
-            int[] chatOffsets = { commonChatOffset, partyChatOffset, whisperChatOffset, allianceChatOffset, clanChatOffset, worldChatOffset, superChatOffset, broadcastChatOffset, otherChatOffset, damageChatOffset, systemChatOffset };
+            var offsets = new chatColorOffsets();
+            int[] chatOffsets = {
+                offsets.commonChatOffset,
+                offsets.partyChatOffset,
+                offsets.whisperChatOffset,
+                offsets.allianceChatOffset,
+                offsets.clanChatOffset,
+                offsets.worldChatOffset,
+                offsets.superChatOffset,
+                offsets.broadcastChatOffset,
+                offsets.otherChatOffset,
+                offsets.damageChatOffset,
+                offsets.systemChatOffset
+            };
 
-            using BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read));
-
-            for (int i = 0; i < chatOffsets.Length; i++)
+            if (File.Exists(file))
             {
-                reader.BaseStream.Seek(chatOffsets[i], SeekOrigin.Begin);
-                byte[] data = reader.ReadBytes(14);
-                string color = Encoding.BigEndianUnicode.GetString(data);
-                Trace.WriteLine(color);
-                content[i].Text = color;
+                using BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read));
+                for (int i = 0; i < chatOffsets.Length; i++)
+                {
+                    reader.BaseStream.Seek(chatOffsets[i], SeekOrigin.Begin);
+                    byte[] data = reader.ReadBytes(14);
+                    string color = Encoding.BigEndianUnicode.GetString(data);
+                    Trace.WriteLine(color);
+                    content[i].Text = color;
+                }
+            } 
+            else
+            {
+                return;
             }
         }
 
@@ -92,18 +115,20 @@ namespace element_configurator
         private static void SetDefaultChatColors()
         {
             // player usable chats 
-            int commonChatOffset = 0x0067C4BB;      // Common Chat		Default Color: ^FFFFFF 0x0067C4BC
-            int partyChatOffset = 0x0067C4CB;       // Party Chat		Default Color: ^00FF00 0x0067C4CC
-            int whisperChatOffset = 0x0067E983;     // Whisper Chat 	Default Color: ^FF7FFF 0x0067E984
-            int allianceChatOffset = 0x0067E993;    // Alliance Chat	Default Color: ^00FFFF
-            int clanChatOffset = 0x0067E943;        // Clan Chat		Default Color: ^14A9FF
-            int worldChatOffset = 0x0067E9A3;       // World Chat		Default Color: ^FFFF00
-            int superChatOffset = 0x0067E933;       // Super Chat		Default Color: ^A900C7
-            int broadcastChatOffset = 0x0067C4AB;   // Broadcast Chat	Default Color: ^FF0000 0x0067C4AC
-            int otherChatOffset = 0x0067E953;       // Other Chat		Default Color: ^B680FF
-            int damageChatOffset = 0x0067E973;      // Damage Chat		Default Color: ^FFAE00
-            int systemChatOffset = 0x0067E963;		// System Chat		Default Color: ^FF7F00
-            int[] chatOffsets = { commonChatOffset, partyChatOffset, whisperChatOffset, allianceChatOffset, clanChatOffset, worldChatOffset, superChatOffset, broadcastChatOffset, otherChatOffset, damageChatOffset, systemChatOffset };
+            var offsets = new chatColorOffsets();
+            int[] chatOffsets = {
+                offsets.commonChatOffset,
+                offsets.partyChatOffset,
+                offsets.whisperChatOffset,
+                offsets.allianceChatOffset,
+                offsets.clanChatOffset,
+                offsets.worldChatOffset,
+                offsets.superChatOffset,
+                offsets.broadcastChatOffset,
+                offsets.otherChatOffset,
+                offsets.damageChatOffset,
+                offsets.systemChatOffset
+            };
             string[] chatColors = { "^FFFFFF", "^00FF00", "^FF7FFF", "^00FFFF", "^14A9FF", "^FFFF00", "^A900C7", "^FF0000", "^B680FF", "^FFAE00", "^FF7F00"};
 
             createBackup("./elementclient.exe");
@@ -135,18 +160,21 @@ namespace element_configurator
                 txtSystemChatColor.Text
             };
 
-            int commonChatOffset = 0x0067C4BB;      // Common Chat		Default Color: ^FFFFFF 0x0067C4BC
-            int partyChatOffset = 0x0067C4CB;       // Party Chat		Default Color: ^00FF00 0x0067C4CC
-            int whisperChatOffset = 0x0067E983;     // Whisper Chat 	Default Color: ^FF7FFF 0x0067E984
-            int allianceChatOffset = 0x0067E993;    // Alliance Chat	Default Color: ^00FFFF
-            int clanChatOffset = 0x0067E943;        // Clan Chat		Default Color: ^14A9FF
-            int worldChatOffset = 0x0067E9A3;       // World Chat		Default Color: ^FFFF00
-            int superChatOffset = 0x0067E933;       // Super Chat		Default Color: ^A900C7
-            int broadcastChatOffset = 0x0067C4AB;   // Broadcast Chat	Default Color: ^FF0000 0x0067C4AC
-            int otherChatOffset = 0x0067E953;       // Other Chat		Default Color: ^B680FF
-            int damageChatOffset = 0x0067E973;      // Damage Chat		Default Color: ^FFAE00
-            int systemChatOffset = 0x0067E963;		// System Chat		Default Color: ^FF7F00
-            int[] chatOffsets = { commonChatOffset, partyChatOffset, whisperChatOffset, allianceChatOffset, clanChatOffset, worldChatOffset, superChatOffset, broadcastChatOffset, otherChatOffset, damageChatOffset, systemChatOffset };
+            // player usable chats 
+            var offsets = new chatColorOffsets();
+            int[] chatOffsets = {
+                offsets.commonChatOffset,
+                offsets.partyChatOffset,
+                offsets.whisperChatOffset,
+                offsets.allianceChatOffset,
+                offsets.clanChatOffset,
+                offsets.worldChatOffset,
+                offsets.superChatOffset,
+                offsets.broadcastChatOffset,
+                offsets.otherChatOffset,
+                offsets.damageChatOffset,
+                offsets.systemChatOffset
+            };
 
             createBackup("./elementclient.exe");
             for (int i = 0; i < chatOffsets.Length; i++)
